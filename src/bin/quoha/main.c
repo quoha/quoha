1b3c3a306887e4b2d5cc7b9bc3415dbd58f47bea
Michael Henderson <quoha@quoha.org>
2011-05-30 17:17:25 -0600

    add in CuTest as our unit testing framework

---
+++ src/bin/quoha/main.c
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ *  Quoha - a quaint framework for static websites
+ *
+ *  Copyright (C) 2011 Michael D Henderson
+ *
+ *  This file is part of Quoha (http://quoha.org).
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	printf("Quoha Copyright (C) 2011  Michael D Henderson\n\n");
+	printf("This program comes with ABSOLUTELY NO WARRANTY; for details\n");
+	printf("restart with the '--show-warranty' option on the command line\n\n");
+	printf("This is free software, and you are welcome to redistribute it\n");
+	printf("under certain conditions; for details, restart with the\n");
+	printf("'--show-copying' option on the command line\n\n");
+
+	return 0;
+}
