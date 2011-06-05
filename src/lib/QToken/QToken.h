651ddd7f412deabe0f21d8dc88f8b44f47ab88a1
Michael Henderson <quoha@quoha.org>
2011-06-05 12:41:29 -0600

    add QToken

---
+++ src/lib/QToken/QToken.h
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ *  Quoha - a quaint framework for static websites
+ *
+ *  Copyright (C) 2011 Michael D Henderson
+ *
+ *  This file is part of Quoha (http://quoha.org).
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+#ifndef Quoha_src_lib_QToken_QToken_H
+#define Quoha_src_lib_QToken_QToken_H
+
+/*****************************************************************************
+ */
+struct QToken {
+	int         column;
+	int         line;
+	const char *source;
+	const char *token;
+};
+typedef struct QToken QToken;
+
+#endif
